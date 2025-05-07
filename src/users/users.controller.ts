9b5ffcef6ba343e0ab393bdfcbef28cdd5f87a41
Hassam <haslam@technologyrivers.com>
2025-05-07 17:13:10 +0500

    create user dto and global validation pipe added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    @Post("signup")
+    createUser(@Body() body: CreateUserDto) {
+        const { email, password } = body;
+        console.log(email, password);
+        // Here you would typically save the user to the database
+        // and return a response.
+        return "User Created";
+    }
+}
