b31d1c7ee020e802399ad5056588d7de86cfe296
Hassam <haslam@technologyrivers.com>
2025-05-27 12:00:53 +0500

    signin method unit tests

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,13 +5,17 @@ import { User } from './user.entity';
 import { BadRequestException } from '@nestjs/common';
 describe('Auth Service', () => {
   let service: AuthService;
-  let FakeUsersService: Partial<UsersService>
+  let FakeUsersService: Partial<UsersService>;
+  let users: User[] = [];
   beforeEach(async () => {
-     FakeUsersService = {
+    FakeUsersService = {
       create: ({ email, password }: { email: string; password: string }) => {
-        return Promise.resolve({ id: 1, email, password } as User);
+        const user = { id: 1, email, password } as User;
+        users.push(user);
+        return Promise.resolve(user);
       },
-      find: () => Promise.resolve([]),
+      find: (email: string) =>
+        Promise.resolve(users.filter((user) => user.email === email)),
     };
     // Create DI container
     const module = await Test.createTestingModule({
@@ -30,12 +34,32 @@ describe('Auth Service', () => {
     expect(service).toBeDefined();
   });
   it('should salt and hash password', async () => {
-      const user = await service.signup("abc@example.com","123");
-      const [salt,hash] = user.password.split(".")
-      expect(hash).toBeDefined();
+    const user = await service.signup('abc@example.com', '123');
+    const [salt, hash] = user.password.split('.');
+    expect(hash).toBeDefined();
   });
-  it("should throw error email already in use", () => {
-    FakeUsersService.find = () => Promise.resolve([{id:1, email:"abc@example.com", password:"123"} as User])
-    expect(service.signup("abc@example.com","123")).rejects.toThrow(BadRequestException)
+  it('should throw error email already in use', async () => {
+    FakeUsersService.find = () =>
+      Promise.resolve([
+        { id: 1, email: 'abc@example.com', password: '123' } as User,
+      ]);
+   await expect(service.signup('abc@example.com', '123')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+  
+  it('should be able to sign in with correct credentials', async () => {
+    
+    const user = await service.signin('abc@example.com', '123');
+    expect(user).toBeDefined();
+  });
+  it("should throw an error if signin is called with an unused email", async () => {
+   
+   await  expect(service.signin('abac@ab.com',"123")).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+  it("should throw an error if an invalid password is provided", async () => {
+   await expect( service.signin("abc@example.com","1234")).rejects.toThrow(BadRequestException)
   })
 });
